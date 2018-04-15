1677dc5b4478d85bedd404980c27ec836fba2034
edwinlee44 <leeweiannedwin@gmail.com>
2018-04-15 17:26:02 +0800

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -6,6 +6,42 @@ function validEmail(email) {
   return re.test(email);
 }
 
+jQuery(document).ready(function() {
+ 
+	var offset = 250;
+	 
+	var duration = 300;
+	 
+	jQuery(window).scroll(function() {
+	 
+		if (jQuery(this).scrollTop() > offset) {
+		 
+			jQuery('.back-to-top').fadeIn(duration);
+		 
+		} else {
+		 
+			jQuery('.back-to-top').fadeOut(duration);
+		 
+		}
+ 
+	})
+
+	jQuery('.back-to-top').click(function(event) {
+ 
+		event.preventDefault();
+		 
+		jQuery('html, body').animate({scrollTop: 0}, "slow");
+		 
+		return false;
+ 
+	});
+});
+ 
+ 
+ 
+
+ 
+
 
 $('#submit-form').on('click', function(e) {
 	
